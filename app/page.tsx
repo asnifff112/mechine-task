f63722f84298e1ac85989bf58f1db62910dc7a95
asnif <asnifkl@gmail.com>
2025-12-23 12:47:06 +0530

    first committ

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"useclient"
+
+import { Canvas,useFrame } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+import { useRef } from "react"
+import * as THREE from "three";
+
+function box (){
+    const meshref = useRef<THREE.Mesh>(null!);
+
+    
+}
