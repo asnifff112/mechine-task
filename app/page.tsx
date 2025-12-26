25f570ad074ec3d64dd9f8dccd7c8339b7547084
asnif <asnifkl@gmail.com>
2025-12-26 11:52:50 +0530

    first committ

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,10 @@ import * as THREE from "three";
 
 function box (){
     const meshref = useRef<THREE.Mesh>(null!);
-
     useFrame((_,delta) => {
         meshref.current.rotation.y +=delta
         meshref.current.rotation.x +=delta
     });
-    
     return (
         <mesh ref={meshref} castShadow receiveShadow>
             <boxGeometry args={[1.5,1.5,1.5]}/>
