e158f485c32970987240696c4005cb4a7d6e11c6
asnif <asnifkl@gmail.com>
2025-12-29 10:02:15 +0530

    re

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { useRef } from "react";
 import * as THREE from "three"
+import React from 'react'
 
 function Box(){
   const meshRef= useRef<THREE.Mesh>(null!)
@@ -12,4 +13,15 @@ function Box(){
     meshRef.current.rotation.y += delta;
     meshRef.current.rotation.x += delta * 0.5;
   });
+
+  return (
+    <mesh ref={meshRef} castShadow receiveShadow>
+      <sphereGeometry args={[1,32,32]}/>
+      <meshStandardMaterial color="blue"/>
+    </mesh>
+  )
+}
+
+export default function Home (){
+
 }
