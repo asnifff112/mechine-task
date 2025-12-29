24bd3e4dcfafdc7d95db0820c21ebb8e03c03622
asnif <asnifkl@gmail.com>
2025-12-29 12:16:14 +0530

    re

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,32 @@
-"use client"
+"use client";
 
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { useRef } from "react";
-import * as THREE from "three"
-import React from 'react'
 
-function Box(){
-  const meshRef= useRef<THREE.Mesh>(null!)
-
-  useFrame((_, delta) => {
-    meshRef.current.rotation.y += delta;
-    meshRef.current.rotation.x += delta * 1;
-  });
-
+export default function Home() {
   return (
-    <mesh ref={meshRef} castShadow receiveShadow>
-      <boxGeometry args={[10,10,10]}/>
-      <meshStandardMaterial color="blue"/>
-    </mesh>
-  )
-}
+    <main className="w-screen h-screen">
+      <Canvas camera={{ position: [3, 3, 6], fov: 50 }}>
+        <ambientLight intensity={0.4} />
+        <directionalLight position={[5, 5, 5]} intensity={1} />
 
-export default function Home (){
- return(
- <main className="w-screen h-screen">
+       
+        <group>
+        
+          <mesh position={[-1.2, 0, 0]}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="orange" />
+          </mesh>
 
-   <Canvas
-     shadows
-     camera={{position:[3,3,6], fov :50}}   
-   >
-    <ambientLight intensity={5}/>
-    <directionalLight
-     position={[5,5,5]}
-     intensity={5}
-     castShadow
-    />
+        
+          <mesh position={[1.2, 0, 0]}>
+            <sphereGeometry args={[0.6, 32, 32]} />
+            <meshStandardMaterial color="skyblue" />
+          </mesh>
+        </group>
 
-   <Box/>
-   <OrbitControls enableZoom/>
-
-   </Canvas>
-
- </main>
- )
+        <OrbitControls />
+      </Canvas>
+    </main>
+  );
 }
