61de93849659a4d7d51350b55c9b306f7adb0789
asnif <asnifkl@gmail.com>
2025-12-29 09:38:07 +0530

    re

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,10 @@
 "use client"
+
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+import * as THREE from "three"
+
+function Box(){
+  const meshref= useRef<THREE.Mesh>(null)
+}
